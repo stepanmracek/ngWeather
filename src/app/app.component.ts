ac5b24eb177767557db375f44383cb2d108e5a27
Stepan Mracek <stepan.mracek@gmail.com>
2017-01-28 23:38:15 +0100

    use localStorage for saving and loading locations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,46 @@ import { Component, OnInit } from '@angular/core';
 
 import { WeatherDownloaderService } from './weather-downloader.service';
 import { Weather } from './iweather';
+import { ISavedLocation } from './isavedlocation';
+
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+	savedLocationsKey: string = 'savedLocations';
+	locations: ISavedLocation[] = [];
+	newLocation: string = null;
+	selectedLocation: string = null;
 	weather: Weather.IWeather;
 	error: any;
 
 	constructor(private weatherService: WeatherDownloaderService) { }
 
 	ngOnInit(): void {
-		this.weatherService.getCurrentWeather('Brno')
-			.subscribe(weather => this.weather = weather, error => this.error = error);
+		var savedLocationsString = localStorage.getItem(this.savedLocationsKey);
+		if (!savedLocationsString) {
+			savedLocationsString = JSON.stringify([]);
+			localStorage.setItem(this.savedLocationsKey, savedLocationsString);
+		}
+		this.locations = JSON.parse(savedLocationsString);
+
+		if (this.locations.length > 0) {
+			this.selectedLocation = this.locations[0].name;
+		}
+
+		/*this.weatherService.getCurrentWeather(this.location)
+			.subscribe(weather => this.weather = weather, error => this.error = error);*/
+	}
+
+	addLocation(): void {
+		if (!this.newLocation) return;
+		var item: ISavedLocation = {
+			name: this.newLocation,
+		}
+		if (this.locations.findIndex(value => value.name === item.name) >= 0) return;
+		this.locations.push(item);
+		localStorage.setItem(this.savedLocationsKey, JSON.stringify(this.locations));
 	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { WeatherDownloaderService } from './weather-downloader.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [
-    WeatherDownloaderService
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		NgbModule.forRoot(),
+	],
+	providers: [
+		WeatherDownloaderService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
